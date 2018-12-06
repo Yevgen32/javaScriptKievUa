a0677cb57bde471032aa92d1a17c3ec2093d81a4
Yevgen32 <dakgo4@gmail.com>
2018-12-06 11:29:24 +0200

    done if-else ex1

--- if-else/1/js/script.js
+++ if-else/1/js/script.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.getElementById('compare').onclick = function () {
+    let n1 = document.getElementById('n1').value;
+    let n2 = document.getElementById('n2').value;
+    let res = document.getElementById('res');
+    if (n1 > n2){
+        res.value = n1;
+    }
+    else if (n1 < n2){
+        res.value = n2;
+    }
+    else {
+       res.value = 'ERROR';
+    }
+    return res.value;
+}
