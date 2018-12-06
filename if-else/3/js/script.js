ba361b92c7acf2af11684b121949e2b578c83634
Yevgen32 <dakgo4@gmail.com>
2018-12-06 11:55:52 +0200

    done if-else ex3

--- if-else/3/js/script.js
+++ if-else/3/js/script.js
@@ -1,1 +1,18 @@
-﻿
+﻿document.getElementById('click').onclick = function () {
+    let n = document.getElementById('n').value;
+    let res = document.getElementById('res');
+    if (n <= 90 && n >= 1){
+        if (n >= 1 && n <= 20){
+            res.value = 'house_1';
+        }
+        else if (n >= 21 && n <= 48){
+            res.value = 'house_2';
+        }
+        else if (n >= 49 && n <= 90){
+            res.value = 'house_3';
+        }
+    }
+    else {
+        res.value = 'enter number in the range from 1...90 ';
+    }
+}
