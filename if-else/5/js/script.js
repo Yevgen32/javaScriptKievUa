dc80fdccf566c6fddd766648b04d10b2a6d25940
Yevgen32 <dakgo4@gmail.com>
2018-12-06 12:08:41 +0200

    done if-else ex4

--- if-else/5/js/script.js
+++ if-else/5/js/script.js
@@ -1,1 +1,11 @@
-﻿
+﻿document.getElementById('click').onclick = function () {
+    let login = document.getElementById('login').value;
+    let password = document.getElementById('password').value;
+    if (login == 'ivan' && password == '334455' || login == 'alex' && password == '777' || login == 'petr' && password == 'b5678'){
+        alert('welcome');
+    }
+    else {
+        alert('error');
+    }
+
+};
